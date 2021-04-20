faf99ca161a759cca39c245863992541f2870be0
VYASAB2 <abhinay.vyas@novartis.com>
2021-04-20 15:18:53 +0530

    CHanged classes

--- ultimatelabeling/class_names.py
+++ ultimatelabeling/class_names.py
@@ -1,8 +1,9 @@
 DEFAULT_CLASS_NAMES = {
- 0: 'car',
- 1: 'truck',
- 2: 'bus',
- 3: 'van',
- 4: 'cyclist',
- 5: 'pedestrian'
+ 0: 'Resting',
+ 1: 'Walking',
+ 2: 'Moving_Exploration',
+ 3: 'Local_Exploration',
+ 4: 'Supported_Rearing',
+ 5: 'Unsupported_Rearing',
+ 6: 'Grooming',
 }
